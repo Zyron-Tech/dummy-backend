0698ca4b83ba15bb2d6cad878ccdad9036843bd5
Peace Mathew <zyrontech101@gmail.com>
2024-09-04 15:47:44 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,13 +1,13 @@
 <?php
 // config/db.php
 
-$host = getenv('DB_HOST'); // e.g., 'db.your-render-service.com'
-$db   = getenv('DB_NAME'); // e.g., 'your_database'
-$user = getenv('DB_USER'); // e.g., 'your_username'
-$pass = getenv('DB_PASS'); // e.g., 'your_password'
+$host = getenv('dpg-crce66lumphs73dkrvs0-a'); // e.g., 'your-database-host.render.com'
+$db   = getenv('dummysite_75qn'); // e.g., 'your_database'
+$user = getenv('dummysite_75qn_user'); // e.g., 'admin'
+$pass = getenv('9rCobo41QicVh4jOm2SoNbMgUk7gH4jQ'); // e.g., 'your_password'
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
