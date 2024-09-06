03f26791a3210b4049d7138d0fef4abb264315c8
Peace Mathew <zyrontech101@gmail.com>
2024-09-06 01:27:18 -0700

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+// config/cors.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight (OPTIONS) request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
