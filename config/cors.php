c7254b8a8fd1216033c74740595be1338b8df9bc
Peace Mathew <zyrontech101@gmail.com>
2024-09-10 16:02:58 -0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,6 @@
 <?php
 // config/cors.php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://zyron-tech.github.io"); // Change '*' to 'https://zyron-tech.github.io' if you want to restrict it to your frontend domain
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
@@ -9,3 +9,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+?>
